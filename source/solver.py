104b1a2adaadfc9a1f9a35054679f31bae33e191
Michael Howland <Mike@dn51s4m3.sunet>
2017-02-19 13:59:11 -0800

    solver

--- source/solver.py
+++ source/solver.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+# Parameters
+# N = 10
+dt = 0.5
+Re = 1000
 
 class Data:
 
@@ -10,37 +14,33 @@ class Data:
 
 
 
-#def solve(...)
-
-
-
-# Solution for u by pressure projection
-# Hardcoded parameters for testing
-nx = 10
-ny = 10
-u = np.zeros((nx, 1))
-D = np.zeros((nx, ny))
-L = np.zeros((nx, ny))
-G = np.zeros((nx, ny))
-dt = 0.5
-Re = 1000.
-P = np.zeros((nx, 1))
-I = np.identity((nx))
-# Semi implicit projection method
-# Solve for u_star
-A1 = I - L/(2.*Re)
-A2 = I + L/(2.*Re)
-rhs = -np.dot(G, P) + np.dot(A2, u)
-u_star = np.linalg.solve(A1, rhs)
-# Pressure correction
-lhsPressure = np.dot(D, G)
-rhsPressure = np.dot(D, u_star)
-P_tild, res, ra, s = np.linalg.lstsq(lhsPressure, rhsPressure)
-# Update velocity and pressure
-GP = np.dot(G, P_tild)
-u = u_star - dt*GP
-P = P + P_tild
-print(u)
-print(P)
-
-
+def solve(data):
+	# Solution for u by pressure projection
+	# Hardcoded parameters for testing
+	u = data.u_vel
+	P = data.press
+	n = len(u)
+	D = np.zeros((n, n))
+	L = np.zeros((n, n))
+	G = np.zeros((n, n))
+	I = np.identity((n))
+	# Semi implicit projection method
+	# Solve for u_star
+	A1 = I - L/(2.*Re)
+	A2 = I + L/(2.*Re)
+	rhs = -np.dot(G, P) + np.dot(A2, u)
+	u_star = np.linalg.solve(A1, rhs)
+	# Pressure correction
+	lhsPressure = np.dot(D, G)
+	rhsPressure = np.dot(D, u_star)
+	P_tild, res, ra, s = np.linalg.lstsq(lhsPressure, rhsPressure)
+	# Update velocity and pressure
+	GP = np.dot(G, P_tild)
+	u = u_star - dt*GP
+	P = P + P_tild
+	print(u)
+	print(P)
+	return Data
+N = 10
+data = Data(N)
+solve(data)
