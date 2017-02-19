a6da4061057f5fc9d0de8bccd54281d4b80ef91c
Michael Howland <Mike@dn51s4m3.sunet>
2017-02-19 14:21:44 -0800

    solver and test

--- source/solver.py
+++ source/solver.py
@@ -1,9 +1,5 @@
 import numpy as np
 
-# Parameters
-# N = 10
-dt = 0.5
-Re = 1000
 
 class Data:
 
@@ -13,22 +9,15 @@ class Data:
                 self.press = np.zeros(N);
 
 
-
-def solve(data):
+def solve(data, D, L, G, Re, dt):
 	# Solution for u by pressure projection
-	# Hardcoded parameters for testing
-	u = data.u_vel
-	P = data.press
-	n = len(u)
-	D = np.zeros((n, n))
-	L = np.zeros((n, n))
-	G = np.zeros((n, n))
+	n = len(data.u_vel)
 	I = np.identity((n))
 	# Semi implicit projection method
 	# Solve for u_star
 	A1 = I - L/(2.*Re)
 	A2 = I + L/(2.*Re)
-	rhs = -np.dot(G, P) + np.dot(A2, u)
+	rhs = -np.dot(G, data.press) + np.dot(A2, data.u_vel)
 	u_star = np.linalg.solve(A1, rhs)
 	# Pressure correction
 	lhsPressure = np.dot(D, G)
@@ -36,11 +25,10 @@ def solve(data):
 	P_tild, res, ra, s = np.linalg.lstsq(lhsPressure, rhsPressure)
 	# Update velocity and pressure
 	GP = np.dot(G, P_tild)
-	u = u_star - dt*GP
-	P = P + P_tild
-	print(u)
-	print(P)
-	return Data
-N = 10
-data = Data(N)
-solve(data)
+	data.u_vel = u_star - dt*GP
+	data.press = data.press + P_tild
+	return data
+
+
+
+
