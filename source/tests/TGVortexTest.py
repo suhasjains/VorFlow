5359fd6dbba10bfc9e136e64a3e8a6b121e8d23d
Aaron Wienkers <awienkers@new.rr.com>
2017-03-03 02:43:18 -0800

    TG vortex test converges

--- source/tests/TGVortexTest.py
+++ source/tests/TGVortexTest.py
@@ -6,12 +6,12 @@ from plotting import *
 from solver import *
 
 
-N = 20**2
+N = 40**2
 L_x = 2.*np.pi
 L_y = 2.*np.pi
-dt = 0.1
-Tend = 1.
-nu = 0.01
+dt = 0.01
+Tend = 0.1
+nu = 0.001
 rho = 1.
 
 mesh = Mesh(N,L_x,L_y,np.zeros(4),'random')
@@ -23,14 +23,22 @@ for i in range(N):
 		data.v_vel[i] = -np.cos(mesh.centroid[i][0])*np.sin(mesh.centroid[i][1])
 
 t = 0.
-plt.ion()
-make_frame(mesh,data.u_vel,'u')
-plt.pause(0.005)
+#plt.ion()
+#make_frame(mesh,data.u_vel,'u')
+#plt.pause(0.005)
 while t < Tend:
 		data = time_step(mesh,data,dt,nu)
-		make_frame(mesh,data.u_vel,'u')
+		#make_frame(mesh,data.u_vel,'u')
+		#plt.pause(0.005)
 		mesh.update_mesh(data, dt)
-		plt.pause(0.005)
 		t += dt
 
 
+u_exact = np.zeros(N);
+for i in range(N):
+	u_exact[i] = np.sin(mesh.centroid[i][0])*np.cos(mesh.centroid[i][1])
+
+print 'Error = '+str(np.sqrt(np.sum(np.dot(np.square(data.u_vel - u_exact),mesh.area))))
+#make_frame(mesh,data.u_vel,'u_final')
+#make_frame(mesh,u_exact,'u_exact')
+make_frame(mesh,data.u_vel - u_exact,'Error')
