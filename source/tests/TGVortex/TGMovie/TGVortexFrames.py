ed5efb5eca97c99c1ac963ab850832d12a14439d
Mike <mike.howland13@gmail.com>
2017-03-14 23:43:33 -0700

    TG frames

--- source/tests/TGVortex/TGMovie/TGVortexFrames.py
+++ source/tests/TGVortex/TGMovie/TGVortexFrames.py
@@ -7,13 +7,13 @@ from solver import *
 import pickle
 
 # AW --
-Nx = 100
+Nx = 150
 N = Nx**2
 L_x = 2.*np.pi
 L_y = 2.*np.pi
 dt = 1./Nx
 dTPlot = 0.05
-Tend = 10.
+Tend = 3.
 nu = 0.01
 rho = 1.
 
@@ -32,19 +32,24 @@ i = 0;
 while t < Tend:
 	if t >= tprint:
 		tprint += dTPlot;
+		#u_exact = np.zeros(N);
+		#for j in range(N):
+			#u_exact[j] = np.sin(mesh.centroid[j][0])*np.cos(mesh.centroid[j][1])* np.exp(-2.*nu*t)
 		u_exact = np.zeros(N);
-		for j in range(N):
-			u_exact[j] = np.sin(mesh.centroid[j][0])*np.cos(mesh.centroid[j][1])* np.exp(-2.*nu*t)
-		
+                errorVec = np.zeros(N)
+                for j in range(N):
+                	u_exact[j] = np.sin(mesh.centroid[j][0])*np.cos(mesh.centroid[j][1])* np.exp(-2.*nu*t)
+                	errorVec[j] = abs(data.u_vel[j] - u_exact[j]) / abs(u_exact[j])
 		#save_frame(mesh,0.5*(data.u_vel**2+data.v_vel**2),'Energy',t,ax,'output/energy'+'{:04d}'.format(i)+'.png',False)
 		#save_frame(mesh,data.press,'Pressure',t,ax,'output/pressure'+'{:04d}'.format(i)+'.png',False)
 		#save_frame(mesh,np.abs(data.u_vel-u_exact),'Error',t,ax,'output/error'+'{:04d}'.format(i)+'.png',False)
-		filename = 'output/output'+'{:04d}'.format(i)+'.p';
+		filename = './outputLocal/output'+'{:04d}'.format(i)+'.p';
 		fileObject = open(filename,'wb')
-		pickle.dump([mesh,data,t,u_exact],fileObject);
+		pickle.dump([mesh,data,t,u_exact, errorVec],fileObject);
 		fileObject.close()
 		i += 1;
 	
 	data = time_step(mesh,data,dt,nu)
         mesh.update_mesh(data, dt)
-        t += dt
+        t+= dt
+	print(t)
